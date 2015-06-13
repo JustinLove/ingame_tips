584a64da7b7d8d13dac8a668acf6290ddc0f9562
Justin Love <git@JustinLove.name>
2015-06-13 11:55:13 -0500

    more tips

--- ui/mods/ingame_tips/tip_catalog.js
+++ ui/mods/ingame_tips/tip_catalog.js
@@ -7,15 +7,71 @@ define([
     tips: [
       {
         id: 'shift-build',
-        text: 'Shift-click factory build icons to add five units',
+        text: 'Shift-click factory build icons to add five units.',
       },
       {
         id: 'continuous-build',
-        text: 'You can set factories to continous build to save constantly requeuing units',
+        text: 'You can set factories to continous build to save constantly requeuing units.',
       },
       {
         id: 'priority-build',
-        text: 'ctrl-clicking a factory build icon adds a unit to the front of the build queue.  If the factory is coninuous, priority units will not be repeated.',
+        text: 'ctrl-clicking a factory build icon adds a unit to the front of the build queue.  If the factory is continuous, priority units will not be repeated.',
+      },
+      {
+        id: 'air-fab',
+        text: 'Air fabricators are less efficient and easily hit by fighters, but movement speed and mobility can be key advantage, especially when alone.',
+      },
+      {
+        id: 'transportable-units',
+        text: 'Air fabricators and naval fabricators are transportable.',
+      },
+      {
+        id: 'transport-limit',
+        text: 'Transport units only hold one unit.',
+      },
+      {
+        id: 'transport-area',
+        text: 'With a group of transport selected, use the load command with an area command (click and drag) to quickly load many units.',
+      },
+      {
+        id: 'teleporter-restrictions',
+        text: 'Only land units can move through a teleporter.',
+      },
+      {
+        id: 'teleporter-linking',
+        text: 'To link teleporters, select one and right click the other.',
+      },
+      {
+        id: 'teleporter-sharing',
+        text: 'You can use other player\'s teleporters. If an enemy, you may need to hold-fire and explicitly choose the "use" command.',
+      },
+      {
+        id: 'fab-assist',
+        text: 'Fabricators can assist each other, as well as factories.',
+      },
+      {
+        id: 'mines',
+        text: 'Only combat bot fabricators and vehicle scouts can see mines.',
+      },
+      {
+        id: 'area-commands',
+        text: 'Many commands can be applied to an area by click and drag. Drag past the horizon to apply to the entire planet.',
+      },
+      {
+        id: 'area-mex',
+        text: 'Metal extractors can be built with area commands. Click and drag to build all spots in the area.',
+      },
+      {
+        id: 'queued-commands',
+        text: 'Hold shift while issuing orders to queue them up.',
+      },
+      {
+        id: 'orbital-radar',
+        text: 'You need an Oribital and Deep Space Radar to see orbital units, unless you have an oribital unit or ground radar nearby.',
+      },
+      {
+        id: 'mex-builders',
+        text: 'Fabricators move faster than your commander, prefer them to roam around building metal extractors.',
       },
     ],
   }
